f32d85088bcb74d828002a43770e6182556c0ca7
YeZawHtet <yezawhtet.web@gmail.com>
2024-02-29 00:27:23 +0630

    .

--- src/MonthList.js
+++ src/MonthList.js
@@ -1,30 +1,43 @@
 import React from "react";
 import "./css/monthList.css";
+import { Carousel } from "react-bootstrap";
 
 export default function MonthList({ months }) {
-  return (
-      <div
-        className="container-fluid bg-dark text-warning"
-        style={{ height: "100vh" }}
-      >
+  // Chunk the months array into arrays of 4 months each
+  const chunkedMonths = [];
+  for (let i = 0; i < months.length; i += 4) {
+    chunkedMonths.push(months.slice(i, i + 4));
+  }
 
-        <div className="row">
-          <div className="col-md-3">{months[0].MonthMm}</div>
-          <div className="col-md-3">{months[1].MonthMm}</div>
-          <div className="col-md-3">{months[2].MonthMm}</div>
-          <div className="col-md-3">{months[3].MonthMm}</div>
-        </div>
-        <div className="row">
-          <div className="col-md-3">{months[4].MonthMm}</div>
-          <div className="col-md-6 text-center">မြန်မာလများ</div>
-          <div className="col-md-3">{months[5].MonthMm}</div>
-        </div>
-        <div className="row">
-          <div className="col-md-3">{months[6].MonthMm}</div>
-          <div className="col-md-3">{months[7].MonthMm}</div>
-          <div className="col-md-3">{months[8].MonthMm}</div>
-          <div className="col-md-3">{months[9].MonthMm}</div>
+  return (
+    <div className="container-fluid">
+      <div className="col-md-12 text-center">
+        <h2>မြန်မာလများ</h2>
+      </div>
+      <div className="row">
+        <div className="col-md-12">
+          <Carousel>
+            {chunkedMonths.map((chunk, index) => (
+              <Carousel.Item key={index}>
+                <div className="row">
+                  {chunk.map((month, idx) => (
+                    <div key={idx} className="col-md-3 ">
+                      <h3>{month.MonthMm}</h3>
+                        <p>{month.MonthEn}</p>
+                        <p>{month.FestivalMm}</p>
+                      <Carousel.Caption>
+                        <h3>{month.MonthMm}</h3>
+                        <p>{month.MonthEn}</p>
+                        <p>{month.FestivalMm}</p>
+                      </Carousel.Caption>
+                    </div>
+                  ))}
+                </div>
+              </Carousel.Item>
+            ))}
+          </Carousel>
         </div>
       </div>
+    </div>
   );
 }
